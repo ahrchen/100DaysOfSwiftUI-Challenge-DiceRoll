591dfde269e54f21e08f2a98588f671bb72e71dd
Raymond Chen <ahrchen@gmail.com>
2022-04-20 11:51:06 -0700

    Initial Commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Raymond Chen on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
